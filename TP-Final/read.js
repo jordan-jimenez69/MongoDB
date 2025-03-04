74337709910f28ace06c06014284b9f7c51b888f
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2025-03-04 09:53:11 +0100

    Add queries to find available books, filter by publication year, author, rating, user location, and genre

--- TP-Final/read.js
+++ TP-Final/read.js
@@ -1,3 +1,17 @@
+//cherchez les livres disponibles
 db.livres.find({ disponible: true })
 
+//cherchez les livres ou année de publi est supérieur à 2000
 db.livres.find({ annee_publication: { $gt: 2000 } })
+
+//cherchez les livres ou auteur est Victor Hugo
+db.livres.find({ auteur: "Victor Hugo" })
+
+//cherchez les livres ou note moyenne est supérieur à 4
+db.livres.find({ note_moyenne: { $gt: 4 } })
+
+//chercher les utilisateurs qui habitent à Paris
+db.utilisateurs.find({ "adresse.ville": "Paris" })
+
+//cherchez les livres ou genre est Drame
+db.livres.find({ genre: "Drame" })
