354ef38877ac34f7109686478046aba3ed2d462b
Jordan JIMENEZ <jordan.jimenez@ecole-isitech.fr>
2025-03-05 11:09:31 +0100

    Add performance improvements and indexing strategies for book queries

---
+++ TP-JOUR2/1.3.js
@@ -0,0 +1,29 @@
+db.livres.createIndex({ titre: "text", description: "text" })
+
+db.livres.find({ $text: { $search: "Livre 1" } }).explain("executionStats")
+
+db.createCollection("sessions_utilisateurs", {
+    validator: {
+        $jsonSchema: {
+            bsonType: "object",
+            required: ["utilisateur_id", "date_derniere_activite", "donnees_session"],
+            properties: {
+                utilisateur_id: {
+                    bsonType: "objectId",
+                    description: "must be an objectId and is required"
+                },
+                date_derniere_activite: {
+                    bsonType: "date",
+                    description: "must be a date and is required"
+                },
+                donnees_session: {
+                    bsonType: "object",
+                    description: "must be an object and is required"
+                }
+            }
+        }
+    }
+})
+
+
+db.sessions_utilisateurs.createIndex({ date_derniere_activite: 1 }, { expireAfterSeconds: 1800 })
